319affc2edf7d603ed445ca84540f8df9693d2cb
Ahsan <johnyhawkahsan@gmail.com>
2023-09-22 15:43:17 +0500

    5th Commit - Alhamdulillah, Firebase auth is working. Able to register and login a user. Now need to implement cardview in my welcome fragment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,12 +41,20 @@ android {
 
 dependencies {
 
+    // UI
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Navigation libraries
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+
+    // Firebase libraries
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+
+    // testing libraries
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
